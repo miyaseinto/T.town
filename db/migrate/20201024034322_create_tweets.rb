c5c6da423cadd5acef288a35f27c242f7c4336ab
miyaseinto <cbj2877@yahoo.co.jp>
2020-10-24 16:17:33 +0900

    第二回目コミット

---
+++ db/migrate/20201024034322_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.string :name
+      t.string :image
+      t.text :text
+      t.timestamps
+    end
+  end
+end
