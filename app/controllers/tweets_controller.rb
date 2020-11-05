d0d0663b04127d7ce8598f64f8f85ad0fedae853
miyaseinto <cbj2877@yahoo.co.jp>
2020-11-05 16:51:18 +0900

    投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,20 @@
 class TweetsController < ApplicationController
 
   def index
+    @tweets = Tweet.all
   end
 
   def new
     @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -13,4 +23,10 @@ class TweetsController < ApplicationController
 
   def edit
   end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
+
