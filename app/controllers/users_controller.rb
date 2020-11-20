014bbff83b8933d5909a31a26706fc8546655082
miyaseinto <cbj2877@yahoo.co.jp>
2020-11-20 13:31:46 +0900

    マイページ設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_user, except: :update
+
+  def show
+    user = User.find(params[:id])
+    @name = user.name
+    @tweets = user.tweets
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to "/users/#{current_user.id}"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :profile_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  resources :users, only: [:show, :edit, :update]
 end
