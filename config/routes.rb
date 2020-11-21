1a434b159dabba10d1989245f945b83ca9e2c730
miyaseinto <cbj2877@yahoo.co.jp>
2020-11-21 23:49:34 +0900

    調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'divisions#index'
+  root to: 'home#index'
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
