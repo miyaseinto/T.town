52c9bfe90b3f801b1f1cb998574678536f8e954c
miyaseinto <cbj2877@yahoo.co.jp>
2020-12-16 15:25:22 +0900

    S3再設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory = 'taijitown2020'
+  config.fog_directory  = 'taijitown2020'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/taijitown2020'
 end
